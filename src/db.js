6f8f1ac9a91b7a3ddb6630eefd68a019bfdab4ee
Paul Merrill <pdm@pdm.me>
2020-03-31 11:39:27 -0700

    sql: Update tables to 2020-03-31

--- src/db.js
+++ src/db.js
@@ -1,11 +1,30 @@
+import { promises as fs } from 'fs';
 import mysql from 'mysql';
 
 const mysqlConfiguration = {
     user: 'root',
     database: 'test',
+
+    multipleStatements: true,
 };
 
 class Conn {
+    static makeConn() {
+        return new Promise((resolve, reject) => {
+            const connection = mysql.createConnection(mysqlConfiguration);
+
+            connection.connect((err) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                const conn = new Conn(connection);
+                resolve(conn);
+            });
+        });
+    }
+
     constructor(connection) {
         this.connection = connection;
         connection.on('error', (err) => {
@@ -42,27 +61,11 @@ class Conn {
     }
 }
 
-function makeConnection() {
-    return new Promise((resolve, reject) => {
-        const connection = mysql.createConnection(mysqlConfiguration);
-
-        connection.connect((err) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-
-            const conn = new Conn(connection);
-            resolve(conn);
-        });
-    });
-}
-
 export async function withConn(res, fn) {
     // Create connection.
     let conn;
     try {
-        conn = await makeConnection();
+        conn = await Conn.makeConn();
     } catch (err) {
         console.error(err);
         res.sendStatus(500);
@@ -81,8 +84,41 @@ export async function withConn(res, fn) {
 
     // End connection.
     try {
-        await conn.end(conn);
+        await conn.end();
     } catch (err) {
         console.error(err);
     }
 }
+
+async function migrate() {
+    const conn = await Conn.makeConn();
+
+    let files = await fs.readdir('./sql/');
+    files.sort();
+    files = files.map((file) => `./sql/${file}`);
+
+    for (const file of files) {
+        if (file.endsWith('.sql')) {
+            console.log(`Applying ${file}:`);
+
+            const buffer = await fs.readFile(file);
+            const content = buffer.toString('utf-8');
+
+            console.log(content);
+
+            await conn.query(content);
+        }
+    }
+    await conn.end();
+}
+
+async function migrateAtBoot() {
+    try {
+        await migrate();
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+migrateAtBoot();
